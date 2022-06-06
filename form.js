fb6cb22dee05bc5bf0902619f358ea37bca551e4
Sampriti Das <99536474+sampritidas@users.noreply.github.com>
2022-06-06 16:58:12 +0530

    Implemented readLine for happy path

--- form.js
+++ form.js
@@ -8,18 +8,10 @@ const main = () => {
     'Enter your BOD',
     'Enter your hobbies'
   ];
-  const form = {};
 
   let index = 0;
-  readline((line) => {
-    console.log(questions[index]);
-    form[questions[index]] = line;
-    console.log(form);
-    if (Object.keys(form).length === 3) {
-      fs.writeFileSync('form.json', JSON.stringify(form), 'utf8');
-    }
-    index++;
-  });
+  console.log(questions[index]);
+  readline(() => x, index);
 }
 
 main();
--- readLine.js
+++ readLine.js
@@ -1,19 +1,80 @@
-const readline = (callBack) => {
-  let input = '';
+const fs = require('fs');
+
+isNameGrant = (name) => {
+  return /^.....*/.test(name);
+};
+
+isDobCorrectlyFormed = (dob) => {
+  console.log(dob);
+  return /^....-..-..$/.test(dob);
+};
+
+const isHobbiesCorrectlyFormed = (hobbies) => {
+  return hobbies.length > 0;
+};
+
+const formHobbies = (chunk) => {
+  return chunk.split(',');
+};
+
+const formInformation = (info) => {
+  return { Name: info[0], DOB: info[1], Hobbies: info[2] };
+};
+
+const readline = (callBack, index) => {
+  const questions = [
+    'Enter your Name',
+    'Enter your DOB',
+    'Enter your Hobbies'
+  ];
+  const details = [];
   process.stdin.setEncoding('utf8');
 
   process.stdin.on('data', chunk => {
-    input += chunk;
-    const slicedInput = input.split('\n');
-    const lines = slicedInput.slice(0, -1);
-    lines.forEach((line) => {
-      callBack(line);
-    });
-    input = slicedInput.slice(-1)[0];
+    console.log(index);
+
+    if (index === 0) {
+      if (!isNameGrant(chunk.trimEnd())) {
+        console.log(questions[index]);
+      } else {
+        details.push(chunk.trimEnd());
+        console.log(questions[index + 1]);
+      }
+    }
+
+    if (index === 1) {
+      if (isDobCorrectlyFormed(chunk)) {
+        console.log(questions[index]);
+      } else {
+        details.push(chunk.trimEnd());
+        console.log(questions[index + 1]);
+      }
+    }
+
+    if (index === 2) {
+      const formedHobbies = formHobbies(chunk);
+      if (!isHobbiesCorrectlyFormed(formedHobbies)) {
+        console.log(questions[index]);
+      } else {
+        details.push(formedHobbies);
+        console.log('hob', details);
+      }
+    }
+
+    if (details.length === questions.length) {
+      fs.writeFileSync(
+        'form.json',
+        JSON.stringify(formInformation(details), null, 2), 'utf8');
+      console.log('Thank you');
+      process.exit();
+    }
+
+    console.log(details);
+    index++;
   });
 
   process.stdin.on('end', () => {
-    callBack(input);
+    console.log('Thank you');
   });
 
   process.stdin.on('close', () => console.log('close'));
